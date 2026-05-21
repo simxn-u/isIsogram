b837619d3b92b681b7159c5ea4b710248c686a7b
simxn-u <simxn@gmx.at>
2026-05-21 14:50:38 +0200

    added isogram function and new unit tests [test]

--- src/isogram.c
+++ src/isogram.c
@@ -5,10 +5,22 @@
 #include <stdio.h>
 
 
+bool seen[256] = {false};
+
 bool is_isogram(const char word[]){
-    // this is the documentation
-    if (false) {
-        printf("hello world\n");
+  if (word == NULL) {
+    return true; // Consider NULL as an isogram
+  }
+
+  for (size_t i = 0; word[i] != '\0'; i++) {
+    char c = word[i];
+    if (c >= 'A' && c <= 'Z') {
+      c += 32; // Convert to lowercase
     }
-    return true;
+    if (seen[(unsigned char)c]) {
+      return false; // Character has been seen before
+    }
+    seen[(unsigned char)c] = true; // Mark character as seen
+  }
+  return true; // All characters are unique
 }
--- test/test_isogram.c
+++ test/test_isogram.c
@@ -8,15 +8,64 @@ void setUp(){}
 void tearDown(){}
 
 // this is a unit test
-void test_empty_string(void) { 
-    TEST_ASSERT_TRUE(is_isogram("")); 
+void test_empty_string(void) {
+  TEST_ASSERT_TRUE(is_isogram(""));
+}
+void test_single_letter(void) {
+  TEST_ASSERT_TRUE(is_isogram("a"));
+}
+void test_repeated_letter(void) {
+  TEST_ASSERT_FALSE(is_isogram("aa"));
+}
+void test_case_insensitivity(void) {
+  TEST_ASSERT_FALSE(is_isogram("Aa"));
+}
+void test_long_isogram(void) {
+  TEST_ASSERT_TRUE(is_isogram("isogram"));
+}
+void test_long_non_isogram(void) {
+  TEST_ASSERT_FALSE(is_isogram("isograms"));
+}
+void test_sc_single_letter(void) {
+  TEST_ASSERT_TRUE(is_isogram("!"));
+}
+void test_sc_repeated_letter(void) {
+  TEST_ASSERT_FALSE(is_isogram("!!"));
+}
+void test_sc_mixed(void) {
+  TEST_ASSERT_FALSE(is_isogram("!aA"));
+}
+void test_sc_mixed_isogram(void) {
+  TEST_ASSERT_TRUE(is_isogram("!a?"));
+}
+void test_sc_mixed_non_isogram(void) {
+  TEST_ASSERT_FALSE(is_isogram("!a?a"));
+}
+void test_number_isogram(void) {
+  TEST_ASSERT_TRUE(is_isogram("12345"));
+}
+void test_number_non_isogram(void) {
+  TEST_ASSERT_FALSE(is_isogram("123451"));
 }
 
 int main(void) {
-    UnityBegin("isIsogram");
+  UnityBegin("isIsogram");
 
-    RUN_TEST(test_empty_string);
+  RUN_TEST(test_empty_string);
+  RUN_TEST(test_single_letter);
+  RUN_TEST(test_repeated_letter);
+  RUN_TEST(test_case_insensitivity);
+  RUN_TEST(test_long_isogram);
+  RUN_TEST(test_long_non_isogram);
+  RUN_TEST(test_long_non_isogram);
+  RUN_TEST(test_sc_single_letter);
+  RUN_TEST(test_sc_repeated_letter);
+  RUN_TEST(test_sc_mixed);
+  RUN_TEST(test_sc_mixed_isogram);
+  RUN_TEST(test_sc_mixed_non_isogram);
+  RUN_TEST(test_number_isogram);
+  RUN_TEST(test_number_non_isogram);
 
-    UnityEnd();
-    return 0;
+  UnityEnd();
+  return 0;
 }
